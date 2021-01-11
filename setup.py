ae31fdeeaa46fff2c1596b316380e2167a2adfcc
Vitaliy Vasilenko <me@vitalvas.com>
2021-01-11 02:44:39 +0200

    Fix package build

--- setup.py
+++ setup.py
@@ -1,46 +1,15 @@
-"""
-PicoDB
-______
+import os
+from setuptools import setup
 
-PicoDB is lightweight, fast, and simple database with namespaces
-
-PicoDB is simple
-````````````````
-
-::
-    >>> import picodb
-
-    >>> db = picodb.load('test.db', False)
-
-    >>> db.set_ns('space')
-    True
-
-    >>> db.set('space', 'key', 'value')
-    True
-
-    >>> db.delete('space', 'key')
-    True
-
-    >>> db.get('space', 'key')
-    'value'
-
-    >>> db.save()
-
-And Easy to Install
-```````````````````
-
-::
-    $ pip install picodb
-
-"""
-
-from distutils.core import setup
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
 
 setup(
     name='PicoDB',
     version='0.1.0',
     description='A lightweight and simple database',
-    long_description=__doc__,
+    long_description=README,
+    long_description_content_type='text/markdown',
     author='VitalVas',
     author_email='source@vitalvas.com',
     url='https://github.com/vitalvas/picodb',
