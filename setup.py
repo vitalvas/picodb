9e394ff3ccd9d033df5c7f655a7f876fb913f820
Vitaliy Vasilenko <me@vitalvas.com>
2021-01-11 03:15:10 +0200

    inc version for build

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
 
 setup(
     name='PicoDB',
-    version='0.1.0',
+    version='0.1.1',
     description='A lightweight and simple database',
     long_description=README,
     long_description_content_type='text/markdown',
